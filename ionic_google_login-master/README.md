c6bd0e4a9408aae50ecb6f291b4828d328adee7b
erwin_sobirin <weynsobhy@gmail.com>
2016-06-07 02:59:10 +0700

    v#	new file:   ionic_google_login-master/www/lib/ionic/scss/_scaffolding.scss

---
+++ ionic_google_login-master/README.md
@@ -0,0 +1,5 @@
+# ionic_google_login
+
+http://excellencenodejsblog.com/cordova-ionic-google-oauth-login-for-your-mobile-app/
+
+https://www.youtube.com/watch?v=867eYcLYUI8
---
+++ ionic_google_login-master/www/js/app.js
@@ -0,0 +1,30 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic','GoogleLoginService'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+.controller('google', function ($scope, googleLogin) {
+            $scope.google_data = {};
+            $scope.login = function () {
+                var promise = googleLogin.startLogin();
+                promise.then(function (data) {
+                    $scope.google_data = data;
+                }, function (data) {
+                    $scope.google_data = data;
+                });
+            }
+        });
